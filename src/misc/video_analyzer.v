2547bd72359edcbc2bbe9f52b83b13b4c0b14ca4
Stefan Voss <75468830+vossstef@users.noreply.github.com>
2026-03-29 22:25:32 +0200

    hdmi sdcard (#10)

--- src/misc/video_analyzer.v
+++ src/misc/video_analyzer.v
@@ -12,7 +12,7 @@ module video_analyzer
  input		  vs,
  input		  de,
  input		  ntscmode,
-
+ input [1:0] screen,  // 0=std, 1=overscan, 2=wide
  output reg[1:0] mode, // 0=ntsc, 1=pal, 2=mono
  output reg	  vreset
 );
@@ -21,16 +21,24 @@ module video_analyzer
 // generate a reset signal in the upper left corner of active video used
 // to synchonize the HDMI video generation to the Atari ST
 reg vsD, hsD;
-reg [11:0] hcnt;    // signal ranges 0..2047
-reg [11:0] hcntL;
+reg [12:0] hcnt;    // signal ranges 0..2047
+reg [12:0] hcntL;
 reg [9:0] vcnt;    // signal ranges 0..313
 reg [9:0] vcntL;
 reg changed;
-
+reg [1:0] screenL;   
+   
 always @(posedge clk) begin
     // ---- hsync processing -----
     hsD <= hs;
     mode <= {1'b0 , ~ntscmode}; // 0=ntsc, 1=pal, 2=mono
+
+    // make sure screen changes in std/overscan/wide also trigger
+    // a vreset
+    if(screen != screenL) begin
+       changed <= 1'b1;
+       screenL <= screen;
+    end
 
     // begin of hsync, falling edge
     if(!hs && hsD) begin
@@ -41,7 +49,7 @@ always @(posedge clk) begin
 
         hcnt <= 0;
     end else
-        hcnt <= hcnt + 12'd1;
+        hcnt <= hcnt + 13'd1;
 
     if(!hs && hsD) begin
        // ---- vsync processing -----
@@ -50,7 +58,8 @@ always @(posedge clk) begin
        if(!vs && vsD) begin
           // check if image height has changed during last cycle
           vcntL <= vcnt;
-          if(vcntL != vcnt) changed <= 1'b1;
+          if(vcntL != vcnt) 
+             changed <= 1'b1;
           vcnt <= 0;  
        end else
          vcnt <= vcnt + 10'd1;
@@ -59,9 +68,8 @@ always @(posedge clk) begin
    // the reset signal is sent to the HDMI generator. On reset the
    // HDMI re-adjusts its counters to the start of the visible screen area
    vreset <= 1'b0;
-   if( 
-       (hcnt == 140 && vcnt == 20 && changed && mode == 2'd1) ||
-       (hcnt == 140 && vcnt == 10 && changed && mode == 2'd0) ) begin
+   if( (hcnt == ((screen==2'd2)?80:(screen==2'd0)?155:140) && vcnt == 20 && changed && mode == 2'd1) ||
+       (hcnt == ((screen==2'd2)?80:(screen==2'd0)?155:140) && vcnt == 10 && changed && mode == 2'd0) ) begin
        vreset <= 1'b1;
        changed <= 1'b0;
    end
--- src/tang/tn20k/c16nano_top.vhd
+++ src/tang/tn20k/c16nano_top.vhd
@@ -142,12 +142,12 @@ signal system_reset   : std_logic_vector(1 downto 0);
 signal disk_reset     : std_logic;
 signal disk_chg_trg   : std_logic;
 signal disk_chg_trg_d : std_logic;
-signal sd_img_size    : std_logic_vector(31 downto 0);
-signal sd_img_size_d  : std_logic_vector(31 downto 0);
-signal sd_img_mounted : std_logic_vector(5 downto 0);
+signal sd_img_size    : std_logic_vector(63 downto 0);
+signal sd_img_size_d  : std_logic_vector(63 downto 0);
+signal sd_img_mounted : std_logic_vector(7 downto 0);
 signal sd_img_mounted_d : std_logic;
-signal sd_rd          : std_logic_vector(5 downto 0);
-signal sd_wr          : std_logic_vector(5 downto 0);
+signal sd_rd          : std_logic_vector(7 downto 0);
+signal sd_wr          : std_logic_vector(7 downto 0);
 signal disk_lba       : std_logic_vector(31 downto 0);
 signal sd_lba         : std_logic_vector(31 downto 0);
 signal loader_lba     : std_logic_vector(31 downto 0);
@@ -170,7 +170,7 @@ signal disk_g64       : std_logic;
 signal disk_g64_d     : std_logic;
 signal c1541_reset    : std_logic;
 signal c1541_osd_reset : std_logic;
-signal system_wide_screen : std_logic;
+signal system_screen  : std_logic_vector(1 downto 0);
 signal system_floppy_wprot : std_logic_vector(1 downto 0);
 signal leds           : std_logic_vector(5 downto 0);
 signal led1541        : std_logic;
@@ -665,7 +665,7 @@ port map(
       mcu_data  => mcu_data_out,
 
       -- values that can be configure by the user via osd
-      system_wide_screen => system_wide_screen,
+      system_screen => system_screen,
       system_scanlines => system_scanlines,
       system_volume => system_volume,
 
@@ -888,7 +888,7 @@ hid_inst: entity work.hid
   system_reset        => system_reset,
   system_scanlines    => system_scanlines,
   system_volume       => system_volume,
-  system_wide_screen  => system_wide_screen,
+  system_screen       => system_screen,
   system_floppy_wprot => system_floppy_wprot,
   system_port_1       => port_1_sel,
   system_port_2       => port_2_sel,
@@ -1202,6 +1202,9 @@ begin
  end if;
 end process;
 
+sd_rd(7 downto 6) <= (others => '0');
+sd_wr(7 downto 6) <= (others => '0');
+
 crt_inst : entity work.loader_sd_card
   port map (
     clk               => clk_sys,
@@ -1217,14 +1220,14 @@ crt_inst : entity work.loader_sd_card
     sd_rd_data        => sd_rd_data,
     sd_rd_byte_strobe => sd_rd_byte_strobe,
   
-    sd_img_mounted    => sd_img_mounted,
+    sd_img_mounted    => sd_img_mounted(5 downto 0),
     loader_busy       => loader_busy,
     load_crt          => load_crt,
     load_prg          => load_prg,
     load_rom          => load_rom,
     load_tap          => load_tap,
     load_flt          => open,
-    sd_img_size       => sd_img_size,
+    sd_img_size       => sd_img_size(31 downto 0),
     leds              => open,
     img_select        => open,
   
